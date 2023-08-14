f754806d39b04fc14d365f7c6c7a5f90d39b40ee
ghiasmaryam <ghiasmaryam09@gmail.com>
2023-08-15 00:33:30 +0500

    Second Commit

--- myfbclone/facebook/views.py
+++ myfbclone/facebook/views.py
@@ -1,32 +1,42 @@
+
 from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User ,auth
+from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 #decorator for user to enter home page after authentication
 @login_required(login_url='login')
+
 #home page
-def HomePage(request):
- return render (request,'home.html') 
+def IndexPage(request):
+ return render (request,'index.html') 
 
 #signup
 
 def SignupPage(request):
     if request.method=='POST':
-       uname=request.POST.get('username')  
-       email=request.POST.get('email')
-       pass1=request.POST.get('password1') 
-       pass2=request.POST.get('password2')
-    
-       if pass1!=pass2:
-        return HttpResponse("PASSWORD NOT MATCHED")
-       else:
-        my_user=User.objects.create_user(uname,email,pass1)
-        my_user.save()
-        return redirect('login')
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        
+         
+        if pass1!=pass2:
+            messages.info(request,'password not match!!')
+        else:
 
-    
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            user_model = User.objects.get(username=uname)
+            new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+            new_profile.save()
+            return redirect('login')
+        
     return render (request,'signup.html')
+
     
 
 #login
@@ -38,9 +48,10 @@ def LoginPage(request):
         user=authenticate(request,username=username,password=pass1)
         if user is not None:
             login(request,user)
-            return redirect('home')
+            return redirect('index')
+         
         else:
-            return HttpResponse ("Incorrect username or password")
+            messages.info(request,'Credentials Invalid')
     
     return render(request,'login.html')
 
@@ -49,5 +60,5 @@ def LoginPage(request):
 #logout button
 
 def LogoutPage(request):
-    logout(request)
-    return redirect('login')
+   auth.logout(request)
+   return redirect('login')
