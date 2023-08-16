f34e39908eece4ada8ba8e1c11a55294e2194d7c
ghiasmaryam <ghiasmaryam09@gmail.com>
2023-08-17 02:13:23 +0500

    image uploading/like/follow/user_profile settings

--- myfbclone/facebook/urls.py
+++ myfbclone/facebook/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('/signup',views.SignupPage,name='signup'),
-    path('/login',views.LoginPage,name='login'),
+    path('signup/',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
     path('',views.IndexPage,name='index'),
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
-
+    path('like-post', views.like_post, name='like-post'),
+    path('profile/<str:pk>',views.profile,name='profile'),
+    path('follow', views.follow, name='follow'),
 
 ]
--- myfbclone/facebook/views.py
+++ myfbclone/facebook/views.py
@@ -4,16 +4,20 @@ from django.contrib.auth.models import User ,auth
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile,Post,LikePost,FollowersCount
 
 #decorator for user to enter home page after authentication
 
 #home page
 @login_required(login_url='login')
 def IndexPage(request):
- user_object=User.objects.get(username=request.user.username)
- user_profile=Profile.objects.get(user=user_object)
- return render (request,'index.html',{'user_profile': user_profile}) 
+ user_object = User.objects.get(username=request.user.username)
+ user_profile = Profile.objects.get(user=user_object)
+
+
+  #to show uploaded post on index
+ posts=Post.objects.all()
+ return render(request, 'index.html', {'user_profile': user_profile,'posts':posts})
 
 #signup
 
@@ -45,7 +49,7 @@ def SignupPage(request):
              user_model = User.objects.get(username=uname)
              new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
              new_profile.save()
-             return redirect('settings')
+             return redirect('login')
         else:
             messages.info(request,'Password not matched')
             return redirect('signup')
@@ -109,4 +113,75 @@ def settings(request):
 
 #uploading_post
 def upload(request):
-    return HttpResponse('<h1> Upload View</h1>')
+    if request.method=='POST':
+        user=request.user.username
+        image=request.FILES.get('image_upload')
+        caption=request.POST['caption']
+        
+        new_post=Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    else:
+        return redirect('/')   
+    
+
+#likes_on_post  
+@login_required(login_url='login')
+def like_post(request):  
+    username=request.user.username
+    post_id=request.GET.get('post_id')
+    
+    post=Post.objects.get(id=post_id)
+    
+    like_filter =LikePost.objects.filter(post_id=post_id,username=username).first()
+    if like_filter==None:
+        new_like =LikePost.objects.create(post_id=post_id,username=username)
+        new_like.save()
+        post.no_of_likes=post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes-1
+        post.save()
+        return redirect('/')
+    
+    #user_profile_screen
+
+
+
+#user_profile_screen
+@login_required(login_url='login')
+def profile(request, pk):
+    user_object= User.objects.get(username=pk)
+    user_profile=Profile.objects.get(user=user_object)
+    user_posts=Post.objects.filter(user=pk)
+    user_post_length=len(user_posts)
+        
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts':user_posts,
+        'user_post_length':user_post_length,
+    }
+    return render(request,'profile,html')
+
+
+#follow_user
+@login_required(login_url='login')
+def follow(request):
+    if request.method =='POST':
+       follower = request.POST['follower']
+       user=request.POST['user'] 
+       
+       if FollowersCount.objects.filter(follower=follower,user=user).first():
+           delete_follower=FollowersCount.objects.get(follower=follower,user=user)
+           delete_follower.delete()
+           return redirect('/profile'+user)
+       else:
+          new_follower=FollowersCount.objects.create(follower=follower,user=user)
+          new_follower.save()
+    else:
+     return redirect('/')   
+    
